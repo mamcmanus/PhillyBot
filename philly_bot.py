92bce8ea4c089d4a73f62064ae86a14f4aad0325
Matt McManus <matt.a.mcmanus@gmail.com>
2016-02-18 16:53:55 -0500

    Consolidated news parsing logic and command handler into 2 functions. Fixed issue with unrecognizable unicode strings in urls.

--- philly_bot.py
+++ philly_bot.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 import boto3
 import json
 import sys
+import time
 import telepot
 import feedparser
 from datetime import datetime, timedelta
@@ -20,54 +21,29 @@ $ python2.7 telegram webhook AWS lambda
 #TODO: Add messages for empty news sets
 
 
-bot = telepot.Bot('BOT KEY')
+bot = telepot.Bot('164003969:AAFYiwUKM6BA0IbaDSx5F9nhiSdkqQtpKvY')
 
-
-eagles_url = 'http://www.philly.com/philly_eagles.rss'
-flyers_url = 'http://www.philly.com/philly_flyers.rss'
-sixers_url = 'http://www.philly.com/philly_sixers.rss'
-phillies_url = 'http://www.philly.com/sports_phillies.rss'
-
-def get_eagles_news():
-	eagles_feed = feedparser.parse(eagles_url)
-	eagles_links = []
-	for entry in eagles_feed.entries:
+##RSS feeds for each team that are being parsed for the latest news
+sources = {'eagles': 'http://www.philly.com/philly_eagles.rss',
+		'flyers': 'http://www.philly.com/philly_flyers.rss',
+		'sixers': 'http://www.philly.com/philly_sixers.rss',
+		'phillies': 'http://www.philly.com/sports_phillies.rss'
+		}
+###Function to parse the news based on whatever command the user sends to the bot
+def get_news(source):
+	feed = feedparser.parse(source)
+	links = []
+	for entry in feed.entries:
 		pub_date = datetime.strptime(entry.published, '%a, %d %b %Y %H:%M:%S %Z')
 		if pub_date > (datetime.now() - timedelta(hours=24)):
-			entry_tuple = (entry.title, entry.link, entry.description, entry.published)
-			eagles_links.append(entry_tuple)
-	return eagles_links
+			entry_tuple = (entry.title.encode('ascii', 'ignore'), 
+				entry.link.encode('ascii', 'ignore'), 
+				entry.description.encode('ascii', 'ignore'), 
+				entry.published.encode('ascii', 'ignore'))
+			links.append(entry_tuple)
+	return links
 
-def get_flyers_news():
-	flyers_feed = feedparser.parse(flyers_url)
-	flyers_links = []
-	for entry in flyers_feed.entries:
-		pub_date = datetime.strptime(entry.published, '%a, %d %b %Y %H:%M:%S %Z')
-		if pub_date > (datetime.now() - timedelta(hours=24)):		
-			entry_tuple = (entry.title, entry.link, entry.description, entry.published)
-			flyers_links.append(entry_tuple)
-	return flyers_links
-
-def get_sixers_news():
-	sixers_feed = feedparser.parse(sixers_url)
-	sixers_links = []
-	for entry in sixers_feed.entries:
-		pub_date = datetime.strptime(entry.published, '%a, %d %b %Y %H:%M:%S %Z')
-		if pub_date > (datetime.now() - timedelta(hours=24)):		
-			entry_tuple = (entry.title, entry.link, entry.description, entry.published)
-			sixers_links.append(entry_tuple)
-	return sixers_links
-
-def get_phillies_news():
-	phillies_feed = feedparser.parse(phillies_url)
-	phillies_links = []
-	for entry in phillies_feed.entries:
-		pub_date = datetime.strptime(entry.published, '%a, %d %b %Y %H:%M:%S %Z')
-		if pub_date > (datetime.now() - timedelta(hours=24)):		
-			entry_tuple = (entry.title, entry.link, entry.description, entry.published)
-			phillies_links.append(entry_tuple)
-	return phillies_links
-
+###All the command and chat handlers
 def start(chat_id):
 	bot.sendMessage(chat_id, text='Hi! I am the Philly Phanatic, the best mascot in all of sports. You can ask me about the latest and greatest news about your favorite Philly sports teams. Type "/help" to see what is possible.')
 
@@ -90,45 +66,19 @@ def help(chat_id):
 def settings(chat_id):
 	bot.sendMessage(chat_id, text="Philly Phanatic cannot be configured via any settings yet. Check back soon!")
 
-def eagles(chat_id):
+def news_command_handler(chat_id, team):
 	bot.sendChatAction(chat_id, action='Typing')
-	url_tuples = get_eagles_news()
+	url_tuples = get_news(sources[team])
 	for url in url_tuples:
 		url_md = dedent('''
 			[{}]({})
 			{}'''.format(url[0], url[1], url[2]))
 		bot.sendMessage(chat_id=chat_id, parse_mode='Markdown', disable_web_page_preview=True, text=url_md)
 
-def flyers(chat_id):
-	bot.sendChatAction(chat_id, action='Typing')
-	url_tuples = get_flyers_news()
-	for url in url_tuples:
-		url_md = dedent('''
-			[{}]({})
-			{}'''.format(url[0], url[1], url[2]))
-		bot.sendMessage(chat_id, parse_mode='Markdown', disable_web_page_preview=True, text=url_md)
 
-def sixers(chat_id):
-	bot.sendChatAction(chat_id, action='Typing')
-	url_tuples = get_sixers_news()
-	for url in url_tuples:
-		url_md = dedent('''
-			[{}]({})
-			{}'''.format(url[0], url[1], url[2]))
-		bot.sendMessage(chat_id, parse_mode='Markdown', disable_web_page_preview=True, text=url_md)
-
-def phillies(chat_id):
-	bot.sendChatAction(chat_id, action='Typing')
-	url_tuples = get_phillies_news()
-	for url in url_tuples:
-		url_md = dedent('''
-			[{}]({})
-			{}'''.format(url[0], url[1], url[2]))
-		bot.sendMessage(chat_id, parse_mode='Markdown', disable_web_page_preview=True, text=url_md)
-
+##Function to handle incoming messages and determine what the user is asking for
 def handle(msg):
     flavor = telepot.flavor(msg)
-
     # normal message
     if flavor == 'normal':
         content_type, chat_type, chat_id = telepot.glance2(msg)
@@ -137,13 +87,13 @@ def handle(msg):
         if command == '/start':
         	start(chat_id)
         elif command == '/eagles':
-        	eagles(chat_id)
+        	news_command_handler(chat_id, 'eagles')
         elif command == '/flyers':
-        	flyers(chat_id)
+        	news_command_handler(chat_id, 'flyers')
         elif command == '/sixers':
-        	sixers(chat_id)
+        	news_command_handler(chat_id, 'sixers')
         elif command == '/phillies':
-        	phillies(chat_id)
+        	news_command_handler(chat_id, 'phillies')
         elif command == '/help':
         	help(chat_id)
         elif command == '/settings':
@@ -181,4 +131,17 @@ def my_handler(event, context):
     print("Received event: " + json.dumps(event, indent=2))
     handle(event['message'])
     return('Hopefully it sent...')
+
+###FOR LOCAL DEV
+# def my_handler(event):
+#     print("Received event: " + json.dumps(event, indent=2))
+#     handle(event)
+
+# bot.notifyOnMessage(my_handler)
+
+# print('Listening ...')
+
+# # Keep the program running.
+# while 1:
+#     time.sleep(10)
 
